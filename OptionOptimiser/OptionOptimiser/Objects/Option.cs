43a64e947dbaf0ac064e3237ff9ed5a08b9a24ad
danielfilippi <113997151+danielfilippi@users.noreply.github.com>
2023-12-01 17:38:55 +0000

    Add files via upload
    Completely refactored whole thing, added classes for position and portfolio too

---
+++ OptionOptimiser/OptionOptimiser/Objects/Option.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OptionOptimiser.Calculators;
+
+namespace OptionOptimiser.Objects
+{
+    internal class Option
+    {
+        private bool call;
+        public char EuroAme;
+        public char PutCall;
+        public char LongShort;
+
+        public DateTime MaturityDate;
+        private int days;
+        private int Steps;
+        private double TimeToMaturity;
+
+        public double Spot;
+        public double Strike;
+
+        public double RiskFreeRate;
+
+        private double Volatility;
+        public double ImpliedVolatility;
+        //If the original volatility and the derived IV are very close to each other is a good sign. It suggests that the IV calculation is aligning well with the market conditions as reflected in the original model.
+
+        public double TheoreticalValue;
+        public double OptionValueWithIV;
+
+        public double thetaDecayDays;
+        public double ValueAfterDaysOfTimeDecay;
+
+        public double UserSetMargin;
+        public double UserSetAdditionalMargin; //if option out of the money
+
+        public double BreakEvenPoint { get; set; }
+        public double MaxWin;
+        public double MaxLoss;
+        public double NetDebitCredit; //negative = debit
+        public double Margin;
+        public double UnderlyingOwned;
+
+        public double delta;
+        public double gamma;
+        public double theta;
+        public double vega;
+        public double rho;
+
+        public static Stock underlying;
+        public Option(string Symbol)
+        {
+            days = SetDaysToMaturity();
+            underlying = new Stock(Symbol, days);
+            Thread.Sleep(1000); //avoid race condition?? weirdness going on
+            Spot = underlying.GetValue();
+            Thread.Sleep(1000);
+            Strike = SetStrikePrice();
+            Volatility = underlying.GetVolatility();
+            RiskFreeRate = 0.05;
+            SetNatureOfOption();
+
+            TimeToMaturity = days / 365.25;
+            Steps = days * 5; //High resolution model
+
+            //FIRST WE CALCULATE THE THEORETICAL OPTION VALUE - THEN WE CAN DERIVE THE IV FROM It
+            TheoreticalValue = BinomialCalculators.BinomialWithDividends(Steps, Spot, Strike, RiskFreeRate, Volatility, TimeToMaturity, PutCall, EuroAme, underlying);
+            ImpliedVolatility = VolatilityCalculators.CalculateImpliedVolatility(Volatility, TheoreticalValue, Spot, Strike, TimeToMaturity, RiskFreeRate, call);
+            OptionValueWithIV = BinomialCalculators.BinomialWithDividends(Steps, Spot, Strike, RiskFreeRate, ImpliedVolatility, TimeToMaturity, PutCall, EuroAme, underlying);
+            //FINANCIALS
+            BreakEvenPoint = FinancialCalculators.FindBreakEven(OptionValueWithIV, Strike, call);
+            MaxLoss = FinancialCalculators.FindMaxLoss(LongShort, PutCall, Strike, OptionValueWithIV);
+            MaxWin = FinancialCalculators.FindMaxProfit(LongShort, PutCall, Strike, OptionValueWithIV);
+            NetDebitCredit = FinancialCalculators.FindNetDebCred(OptionValueWithIV, LongShort);
+            Margin = FinancialCalculators.CalcPossibleMargin(LongShort, PutCall, Spot, Strike);
+            //GREEKS
+            delta = GreekCalculators.CalcDelta(OptionValueWithIV, Steps, Spot, Strike, RiskFreeRate, ImpliedVolatility, TimeToMaturity, PutCall, EuroAme, underlying);
+            gamma = GreekCalculators.CalcGamma(OptionValueWithIV, Steps, Spot, Strike, RiskFreeRate, ImpliedVolatility, TimeToMaturity, PutCall, EuroAme, delta, underlying);
+            theta = GreekCalculators.CalcTheta(OptionValueWithIV, Steps, Spot, Strike, RiskFreeRate, ImpliedVolatility, TimeToMaturity, PutCall, EuroAme, underlying);
+            vega = GreekCalculators.CalcVega(OptionValueWithIV, Steps, Spot, Strike, RiskFreeRate, ImpliedVolatility, TimeToMaturity, PutCall, EuroAme, underlying);
+            rho = GreekCalculators.CalcRho(OptionValueWithIV, Steps, Spot, Strike, RiskFreeRate, ImpliedVolatility, TimeToMaturity, PutCall, EuroAme, underlying);
+
+
+            //SetTheta();
+            //ValueAfterDaysOfTimeDecay = SetThetaDecay(OptionValueWithIV);
+        }
+        private void SetNatureOfOption()
+        {
+            Console.WriteLine("L for Long (Buy option), S for Short (Sell option). Caps don't matter");
+            LongShort = char.Parse(Console.ReadLine().ToUpper());
+            Console.WriteLine("E for European, A for American. Caps don't matter");
+            EuroAme = char.Parse(Console.ReadLine().ToUpper());
+            Console.WriteLine("P for Put, C for Call. Caps don't matter");
+            PutCall = char.Parse(Console.ReadLine().ToUpper());
+            if (PutCall == 'C') call = true; //lol
+            else call = false;
+        }
+        private int SetDaysToMaturity()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the maturity date for the option in DD/MM/YYYY format: ");
+                string input = Console.ReadLine();
+                if (DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime maturityDate))
+                {
+                    DateTime today = DateTime.Today;
+                    MaturityDate = maturityDate;
+                    if (maturityDate < today) Console.WriteLine("The entered date is in the past. Please enter a future date.");
+                    else return (maturityDate - today).Days;
+                }
+                else Console.WriteLine("Invalid date format. Please enter the date in DD/MM/YYYY format.");
+            }
+        }
+        private static double SetStrikePrice()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter desired strike price: ");
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double strikePrice))
+                {
+                    if (strikePrice > 0) return strikePrice;
+                    else Console.WriteLine("Please enter a positive number for the strike price.");
+                }
+                else Console.WriteLine("Invalid input. Please enter a numeric value.");
+            }
+        }
+        public override string ToString()
+        {
+            string MaxWinString = "";
+            if (MaxWin == double.PositiveInfinity) MaxWinString = "Unlimited";
+            string MaxLossString = "";
+            if (MaxLoss == double.NegativeInfinity) MaxLossString = "Unlimited";
+
+            string o = LongShort.ToString() + "" + EuroAme.ToString() + " " + PutCall.ToString() + " " + underlying.GetSym() + "\n" +
+                           "Maturity date (days away): " + days + "\n" +
+                           "Spot price: " + Spot + " " + underlying.GetCurrency() + "\n" +
+                           "Strike price: " + Strike + "\n" +
+                           "Risk Free Rate: " + RiskFreeRate + "\n" +
+                           "Implied Volatility: " + ImpliedVolatility + "\n" +
+                           "Option Value: " + OptionValueWithIV + "\n" +
+                           //"One day theta: " + theta + "\n" +
+                           //"Option value after " + thetaDecayDays + " days of theta decay: " + ValueAfterDaysOfTimeDecay + "\n"
+                           "Break even point: " + BreakEvenPoint + " " + underlying.GetCurrency() + "\n" +
+                           "Max Win: " + MaxWinString + "\n" +
+                           "Max Loss: " + MaxLossString + "\n" +
+                           "Net Debit/Credit: (Debit is negative) " + NetDebitCredit + "\n" +
+                           "Total margin to be paid in stock or cash: " + Margin + "\n\n" +
+                           "Delta: " + delta + "\n" +
+                           "Gamma (NOT WORKING): " + gamma + "\n" +
+                           "Theta: " + theta + "\n" +
+                           "Vega: " + vega + "\n" +
+                           "Rho: " + rho + "\n";
+
+            return o;
+        }
+        public double GetBreakEvenPoint()
+        {
+            return BreakEvenPoint;
+        }
+        public char GetPutCall()
+        {
+            return PutCall;
+        }
+        public char GetLongShort()
+        {
+            return LongShort;
+        }
+        public double GetSpot()
+        {
+            return Spot;
+        }
+        public DateTime GetMaturityDate()
+        {
+            return MaturityDate;
+        }
+        public double GetValue()
+        {
+            return OptionValueWithIV;
+        }
+        public double GetMaxWin()
+        {
+            return MaxWin;
+        }
+        public double GetMaxLoss()
+        {
+            return MaxLoss;
+        }
+        public double GetNetDebCred()
+        {
+            return NetDebitCredit;
+        }
+        public double GetMargin()
+        {
+            return Margin;
+        }
+    }
+}
---
+++ OptionOptimiser/OptionOptimiser/OptionOptimiser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionOptimiser
+{
+    internal class OptionOptimiser
+    {
+        //so,  the option value is a proxy for its potential contribution to any portfolio's performance.
+
+
+    }
+}
